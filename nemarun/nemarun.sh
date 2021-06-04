988dc2f6b54aa847728a2d940e81e7f9b6cfc966
Sam Wachspress <samuel.wachspress@northwestern.edu>
2021-06-04 16:13:55 -0500

    use full path for nextflow

--- nemarun/nemarun.sh
+++ nemarun/nemarun.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nextflow run ./gcp.nf -profile gcp --trait_file ${1}/example_trait.tsv --out ${1}/results
+/nemarun/nextflow run ./gcp.nf -profile gcp --trait_file ${1}/example_trait.tsv --out ${1}/results
