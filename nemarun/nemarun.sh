66dd33c8a7a6bf7d1b30fb62ed949aa7807a24d5
Sam Wachspress <samuel.wachspress@northwestern.edu>
2021-06-04 18:03:52 -0500

    fetch pipeline files

--- nemarun/nemarun.sh
+++ nemarun/nemarun.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-/nemarun/nextflow run ./gcp.nf -profile gcp -log /var/log --trait_file ${1}/example_trait.tsv --out ${1}/results -resume
+# Fetch Pipeline Files
+
+wget https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/gcp.nf \
+     https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/nextflow.config
+
+# Fetch Pipeline Config Files
+mkdir conf
+cd conf
+wget https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/annotations.config \
+     https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/gcp.config \
+     https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/mappings.config \
+     https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/mappings_docker.config \
+     https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/simulations.config
+
+cd /nemarun
+/nemarun/nextflow -log /var/log run ./gcp.nf -profile gcp --trait_file ${1}/example_trait.tsv --out ${1}/results -resume
