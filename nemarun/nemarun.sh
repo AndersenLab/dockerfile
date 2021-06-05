7b8fb1e23e27ff3798ad2fc68ac097a09916c59f
Sam Wachspress <samuel.wachspress@northwestern.edu>
2021-06-05 14:54:22 -0500

    delete/redownload version

--- nemarun/nemarun.sh
+++ nemarun/nemarun.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-# Fetch Pipeline Files
+if [ -n "${2}" ] #pipeline version passed as argument, download that version
+  then
+    rm -rf ./gcp.nf ./nextflow.config
+    rm -rf ./conf
+    mkdir conf
+    # Fetch Pipeline Files
+    wget https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/gcp.nf \
+        https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/nextflow.config
 
-wget https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/gcp.nf \
-     https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/nextflow.config
-
-# Fetch Pipeline Config Files
-mkdir conf
-cd conf
-wget https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/annotations.config \
-     https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/gcp.config \
-     https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/mappings.config \
-     https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/mappings_docker.config \
-     https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/simulations.config
+    # Fetch Pipeline Config Files
+    cd conf
+    wget https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/annotations.config \
+        https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/gcp.config \
+        https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/mappings.config \
+        https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/mappings_docker.config \
+        https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/simulations.config
+fi
 
 cd /nemarun
 /nemarun/nextflow -log /var/log run ./gcp.nf -profile gcp --trait_file ${1}/example_trait.tsv --out ${1}/results -resume
