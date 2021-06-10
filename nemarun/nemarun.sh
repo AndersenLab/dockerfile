1148b2b61cc2be58b354d12663ed1714e6198db5
Sam Wachspress <samuel.wachspress@northwestern.edu>
2021-06-09 21:27:05 -0500

    update containers for pipelines

--- nemarun/nemarun.sh
+++ nemarun/nemarun.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-if [ -n "${2}" ] #pipeline version passed as argument, download that version
-  then
-    rm -rf /nemarun/gcp.nf /nemarun/nextflow.config
-    rm -rf /nemarun/conf
-    mkdir /nemarun/conf
-    cd /nemarun
-    # Fetch Pipeline Files
-    wget https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/gcp.nf \
-        https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/nextflow.config
-
-    # Fetch Pipeline Config Files
-    cd conf
-    wget https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/annotations.config \
-        https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/gcp.config \
-        https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/mappings.config \
-        https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/mappings_docker.config \
-        https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/simulations.config
+if [ -n "${2}" ] #pipeline version passed as argument, download that version, otherwise download latest
+  then 
+    VERSION="${2}"
+  else
+    VERSION="cendr-dev"
 fi
 
+rm -rf /nemarun/gcp.nf /nemarun/nextflow.config
+rm -rf /nemarun/conf
+mkdir /nemarun/conf
 cd /nemarun
-/nemarun/nextflow -log /var/log run ./gcp.nf -profile gcp --trait_file ${1}/example_trait.tsv --out ${1}/results -resume
+# Fetch Pipeline Files
+wget https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/gcp.nf \
+    https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/nextflow.config
+
+# Fetch Pipeline Config Files
+cd conf
+wget https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/annotations.config \
+    https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/gcp.config \
+    https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/mappings.config \
+    https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/mappings_docker.config \
+    https://raw.githubusercontent.com/northwestern-mti/NemaScan/${2}/src/conf/simulations.config
+  
+
+
+cd /nemarun
+/nemarun/nextflow run ./gcp.nf -profile gcp --trait_file ${1}/data.tsv --out ${1}/results
