0e708d88346885b15b5e07ecf46eead4ab76792e
Sam Wachspress <samuel.wachspress@northwestern.edu>
2021-06-11 19:04:57 -0500

    fix paths

--- nemarun/nemarun.sh
+++ nemarun/nemarun.sh
@@ -11,11 +11,11 @@
 #
 #
 #   example: 'nemarun.sh d37a6511ac5170d3bf7d31d17ba872e0 gs://elegansvariation.org/reports/nemascan gs://nf-pipelines/workdir'
-#   result: nextflow run ./gcp.nf 
+#   result: nextflow run gcp.nf 
 #                   -profile gcp 
 #                   --trait_file gs://elegansvariation.org/reports/nemascan/d37a6511ac5170d3bf7d31d17ba872e0/data.tsv 
 #                   --out gs://elegansvariation.org/reports/nemascan/d37a6511ac5170d3bf7d31d17ba872e0/results
 #                   --workDir gs://nf-pipelines/workdir/d37a6511ac5170d3bf7d31d17ba872e0
 #   
 #
-nextflow run ./gcp.nf -profile gcp --trait_file ${2}/${1}/data.tsv --out ${2}/${1}/results --workDir ${3}/${1}
+nextflow run gcp.nf -profile gcp --trait_file ${2}/${1}/data.tsv --out ${2}/${1}/results --workDir ${3}/${1}
