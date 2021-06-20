0470699c1d4f0cd3cb4b6015ffcd0d9d9f3a2aaf
Sam Wachspress <samuel.wachspress@northwestern.edu>
2021-06-19 19:56:39 -0500

    update script info

--- nemarun/nemarun.sh
+++ nemarun/nemarun.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 #
 # This script acts as a wrapper around the execution of Nextflow to convert environment variables
-#    into named parameters
-#   result: nextflow run gcp.nf 
-#                   -profile gcp 
-#                   --trait_file gs://elegansvariation.org/reports/nemascan/d37a6511ac5170d3bf7d31d17ba872e0/data.tsv 
-#                   --out gs://elegansvariation.org/reports/nemascan/d37a6511ac5170d3bf7d31d17ba872e0/results
-#                   --workDir gs://nf-pipelines/workdir/d37a6511ac5170d3bf7d31d17ba872e0
+#  into execution parameters
 #   
 #
+
+
 if [[ -z "${TRAIT_FILE}" ]]; then
   echo "TRAIT_FILE environment variable must be set to the Google Storage path of the data"
   exit 1
 fi
 
 if [[ -z "${OUTPUT_DIR}" ]]; then
-  echo "OUTPUT_DIR environment variable must be set to the Google Storage path of the data"
+  echo "OUTPUT_DIR environment variable must be set to the Google Storage path of the output directory"
   exit 1
 fi
 
 
 if [[ -z "${WORK_DIR}" ]]; then
-  echo "OUTPUT_DIR environment variable must be set to the Google Storage path of the data"
+  echo "OUTPUT_DIR environment variable must be set to the Google Storage path of the working directory"
   exit 1
 fi
 
